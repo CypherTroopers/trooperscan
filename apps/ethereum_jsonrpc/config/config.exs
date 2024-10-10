771fe70bcf289d34a353314b4d2a051f4d953436
CypherTroopers <jin360life@gmail.com>
2024-10-10 15:24:52 +0700

    Update config.exs

--- apps/ethereum_jsonrpc/config/config.exs
+++ apps/ethereum_jsonrpc/config/config.exs
@@ -10,8 +10,9 @@ config :ethereum_jsonrpc, EthereumJSONRPC.RequestCoordinator,
   max_jitter: :timer.seconds(2)
 
 config :ethereum_jsonrpc,
-  rpc_transport: if(System.get_env("ETHEREUM_JSONRPC_TRANSPORT", "http") == "http", do: :http, else: :ipc),
-  ipc_path: System.get_env("IPC_PATH")
+  rpc_transport: if(System.get_env("CYSPHERIUM_JSONRPC_TRANSPORT", "http") == "http", do: :http, else: :ipc),
+  ipc_path: System.get_env("IPC_PATH"),
+  rpc_url: System.get_env("RPC_URL") || "https://pubnodes.cypherium.io/"
 
 # Add this configuration to add global RPC request throttling.
 # throttle_rate_limit: 250,
@@ -22,9 +23,9 @@ config :ethereum_jsonrpc,
 # ]
 
 config :ethereum_jsonrpc, EthereumJSONRPC.Tracer,
-  service: :ethereum_jsonrpc,
+  service: :cypherium_jsonrpc,
   adapter: SpandexDatadog.Adapter,
-  trace_key: :blockscout
+  trace_key: :cypheriumscan
 
 config :logger, :ethereum_jsonrpc,
   # keep synced with `config/config.exs`
@@ -32,7 +33,7 @@ config :logger, :ethereum_jsonrpc,
   metadata:
     ~w(application fetcher request_id first_block_number last_block_number missing_block_range_count missing_block_count
        block_number step count error_count shrunk import_id transaction_id)a,
-  metadata_filter: [application: :ethereum_jsonrpc]
+  metadata_filter: [application: :cypherium_jsonrpc]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
